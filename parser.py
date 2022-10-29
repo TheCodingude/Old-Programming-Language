5a86210b0560ce7eb0079c9b117e2e9452252529
Codingdude <sharpdt06@gmail.com>
2022-10-28 22:08:17 -0400

    uhh yes

---
+++ parser.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Tokens:
+
+    VAR = "var"
+    INT = "int"
+    FLOAT = "float"
+    STRING = "string"
+    CHAR = "char"
+    CONST = "const"
+    IF = "if"
+    WHILE = "while"
+    FOR = "for"
+    SWITCH = "switch"
+    CASE = "case"
+    FUNCTION = "function"
+    CLASS = "class"
+    STRUCT = "struct"
+    THIS = "this"
+    IMPORT = "import"
+    FROM = "from"
+    
+
+
